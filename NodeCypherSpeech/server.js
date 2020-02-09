e20682c90886042383c120fa20debe19eb5b0974
dgamliel <dgamliel@DESKTOP-UTT8GF2.localdomain>
2020-02-09 10:28:34 -0800

    SOCKET IO CONNECTION WORKSgit status! :)

--- NodeCypherSpeech/server.js
+++ NodeCypherSpeech/server.js
@@ -1,8 +1,6 @@
-var express = require('express');
-var http    = require('http');
+var app = require('express')();
+var http    = require('http').createServer(app);
 var io      = require('socket.io')(http);
-
-var app = express();
 
 var port = 8082;
 
@@ -16,4 +14,8 @@ io.on('connection', function(socket) {
     console.log("A user connected!");
 });
 
-app.listen(port, () => console.log(`Running on port ${port}`))
+io.on('disconnect', () => {
+    console.log("User disconnected");
+})
+
+http.listen(port, () => console.log(`Running on port ${port}`))

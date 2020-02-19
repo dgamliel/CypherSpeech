ccc0f76260bff40cc073590163826044e9de7ede
Zain Aleshaiker <zalind@gmail.com>
2020-02-18 19:44:28 -0800

    Added WebRTC connection between two clients

--- NodeCypherSpeech/main.js
+++ NodeCypherSpeech/main.js
@@ -1,8 +1,12 @@
 'use strict';
 
+var userName;
+var stream;
+var peerConnection;
+
 //On Page load, create an io socket connection to the server
 //On click of the connect button --> fetch value from user bar and send to server
-//TODO: Implement response from the server s.t. a resposne from the server constitutes a new button on the screen
+//TODO: Implement response from the server s.t. a response from the server constitutes a new button on the screen
 
 $(function() {
 	//const player = document.getElementById('player');
@@ -30,11 +34,13 @@ $(function() {
 
 				recorder.ondataavailable = event => {
 					const blob = event.data;
-					console.log("Blob data", blob);
+					// console.log("Blob data", blob);
 				}
 			
 				//Every 1 ms our data available event goes off
 				recorder.start(1);
+
+				stream = mediaStream;
 		}
 
 	).catch(err => {console.log(err)});
@@ -46,7 +52,7 @@ $(function () {
 	var socket = io();
 
 	$('#connect').click(function () {
-			var userName = $("#username").val();
+			userName = $("#username").val();
 			socket.emit('message', userName);
 			$("#username").val('');
 			$("#connect").prop('disabled', true);
@@ -57,13 +63,125 @@ $(function () {
 		console.log("Adding online user", data);
 		var listData = `<button id="${data}" class="btn btn-lg btn-primary spacingTop spacingLeft">${data}</button>`
 		$("#users").append(listData);
+		
+		$(`#${data}`).click(function () {
+			var request = {
+				from: userName,
+				to: data
+			};
+			socket.emit('request', request);
+		});
+
+		$(`#${userName}`).prop('disabled', true);
 	});
 
-	socket.on('remove', (data) => {
+	socket.on('request-broadcast', data => {
+		//If request is not meant for current user, ignore it
+		if (data.to !== userName) return;
+
+		console.log('connection request', data);
+		if (confirm(data.from + ' would like to connect with you, do you want to connect?')) {
+			socket.emit('initialize', data);
+			makeCall(socket, data);
+		}
+	});
+
+	socket.on('initialize-broadcast', data => {
+		if (data.from !== userName) return;
+		makeCall(socket, data);
+	});
+
+	socket.on('offer-broadcast', async data => {
+		if (data.to !== userName) return;
+		await peerConnection.setRemoteDescription(data.offer);
+		const answer = await peerConnection.createAnswer();
+		await peerConnection.setLocalDescription(answer);
+		
+		var d = {
+			from: data.from,
+			to: data.to,
+			answer: answer
+		};
+		
+		socket.emit('answer', d);
+	});
+
+	socket.on('answer-broadcast', async data => {
+		if (data.from !== userName) return
+		await peerConnection.setRemoteDescription(data.answer);
+	});
+
+	socket.on('icecandidate-broadcast', async data => {
+		await peerConnection.addIceCandidate(data.candidate);
+	});
+
+	socket.on('remove', data => {
 		console.log("User disconnected", data);
 		var removeName = data;
 		$(`#${removeName}`).remove();
-	})
-		
-
+	});
 })
+
+async function makeCall(socket, data) {
+	var config = {iceServers: [{urls: [
+		'stun:stun.l.google.com:19302',
+		'stun:stun1.l.google.com:19302'
+	]}]};
+	
+	peerConnection = new RTCPeerConnection(config);
+	// var remoteConnection = new RTCPeerConnection(config);
+	
+	if (data.from === userName) {
+		peerConnection.onicecandidate = async event => {
+			console.log('idk what this is');
+			console.log(event);
+
+			if (event.candidate) {
+				var d = {
+					from: data.from,
+					to: data.to,
+					candidate: event.candidate
+				};
+
+				socket.emit('icecandidate', d);
+				// await remoteConnection.addIceCandidate(event.candidate);
+			}
+		};
+	}
+
+	peerConnection.ontrack = event => {
+		const recorder = new MediaRecorder(event.streams[0]);
+
+			recorder.ondataavailable = event => {
+				const blob = event.data;
+				// console.log("Blob data", blob);
+			}
+		
+			//Every 1 ms our data available event goes off
+			recorder.start(1);
+	};
+
+	stream.getTracks().forEach(track => {
+		peerConnection.addTrack(track, stream);
+		console.log('track', track);
+	});
+	
+	if (data.from === userName) {
+		const offer = await peerConnection.createOffer();
+		await peerConnection.setLocalDescription(offer);
+
+		var d = {
+			from: data.from,
+			to: data.to,
+			offer: offer
+		};
+
+		socket.emit('offer', d);
+	}
+
+	// await remoteConnection.setRemoteDescription(offer);
+	// const answer = await remoteConnection.createAnswer();
+	// await remoteConnection.setLocalDescription(answer);
+	
+	// await peerConnection.setRemoteDescription(answer);
+}
--- NodeCypherSpeech/server.js
+++ NodeCypherSpeech/server.js
@@ -19,25 +19,51 @@ app.use('/main.js', express.static(path.join(__dirname, '/main.js')));
 io.on('connection', function(socket) {
     console.log("A user connected!");
 		
-		//Users that have connected after things have been posted should get
-		//Everything that has already been posted
-		map.forEach( (valueItem, keyItem, map) => {
-			console.log("key: some socket...", "value", valueItem);
-			socket.emit('response', valueItem);
-		});
+	//Users that have connected after things have been posted should get
+	//Everything that has already been posted
+	map.forEach( (valueItem, keyItem, map) => {
+		console.log("key: some socket...", "value", valueItem);
+		socket.emit('response', valueItem);
+	});
 
-		//On receveing a message
+	socket.on('request', data => {
+		console.log('connection request for', data);
+		io.emit('request-broadcast', data);
+	});
+
+	socket.on('initialize', data => {
+		console.log('initializing connection between', data.from, 'and', data.to);
+		io.emit('initialize-broadcast', data);
+		map
+	});
+
+	socket.on('offer', data => {
+		io.emit('offer-broadcast', data);
+	});
+
+	socket.on('answer', data => {
+		io.emit('answer-broadcast', data);
+	});
+
+	socket.on('icecandidate', data => {
+		io.emit('icecandidate-broadcast', data);
+	});
+
+	//On receveing a message
     socket.on('message', (data) => {
-			map.set(socket, data);
-			var response = `${data}`
-			console.log("Sending ...", data);
-			io.emit('response', response);
+		if (users.has(data)) return;
+		map.set(socket, data);
+		users.add(data);
+		var response = `${data}`
+		console.log("Sending ...", data);
+		io.emit('response', response);
     });
 
     socket.on('disconnect', () => {
-				var userName = map.get(socket);
-				io.emit('remove', userName); 
-				map.delete(socket);
+		var userName = map.get(socket);
+		io.emit('remove', userName); 
+		map.delete(socket);
+		users.delete(userName);
         console.log(`User ${userName} disconnected`);
     });
 });

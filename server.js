80a4e4b72d27581366180e325705ba0579af086c
dgamliel <dgamliel@umail.ucsb.edu>
2020-02-25 13:14:29 -0800

    change port so that heroku can run lol

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var server = require('http').Server(app);
 var path = require('path')
 var io = require('socket.io')(server);
-var port = 8082;
+var port = 80;
 
 var users = new Set();
 var map   = new Map();

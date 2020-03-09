d9ae64b20ecf25b3d03ab27b1cb252d6881b0e7b
dgamliel <dgamliel@DESKTOP-UTT8GF2.localdomain>
2020-03-09 16:34:01 -0700

    added TURN server config so let's see if it works

--- main.js
+++ main.js
@@ -185,16 +185,38 @@ $(function () {
 })
 
 async function makeCall(socket, data) {
-	var config = {
-		iceServers: [{
-			urls: [
-				'stun:stun.l.google.com:19302',
-				'stun:stun1.l.google.com:19302'
-			]
-		}]
-	};
+	// var config = {
+	// 	iceServers: [{
+	// 		urls: [
+	// 			'stun:stun.l.google.com:19302',
+	// 			'stun:stun1.l.google.com:19302',				
+	// 		]
+	// 	}]
+	// };
 
-	peerConnection = new RTCPeerConnection(config);
+	//JSON object with iceServers that map to another JSON object with URLS to TURN and STUN servers
+	var ICE_CONFIG = {
+		iceServers: [
+			{
+				urls:'stun:stun.l.google.com:19302'
+			},
+			{
+				urls:'stun:stun1.l.google.com:19302'
+			},
+			{
+				urls: 'turn:192.158.29.39:3478?transport=udp',
+				credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+				username: '28224511:1379330808'
+			},
+			{
+				urls: 'turn:192.158.29.39:3478?transport=tcp',
+				credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+				username: '28224511:1379330808'
+			}
+		]
+	}
+
+	peerConnection = new RTCPeerConnection(ICE_CONFIG);
 	// var remoteConnection = new RTCPeerConnection(config);
 
 	if (data.peer === userName) {
